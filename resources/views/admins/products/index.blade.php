90d2c52ac989a7bb41f9be7ea0cebb360f0ca7e3
ntrung206 <ntrungcntt2@gmail.com>
2015-12-23 19:09:02 +0700

    delete project

--- resources/views/admins/products/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('admins.admin_template')
-@section('content')
-{!! Former::open() !!}
-{!! Former::text('name_products_categories')->required()!!}
-
-{!! Former::textarea('description')->value(NULL)->plaseholder('please input')->required('abcde') !!}
-{!! Former::submit('btn-primary') !!}
-{!! Former::close() !!}
-@stop
